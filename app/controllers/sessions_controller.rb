4649be54dca961aff77755534095143450a8357d
Dmitrii Iurin <dimon07710@yandex.ru>
2019-08-18 21:51:07 +0300

    add User authorization and authentication

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    @user = User.authenticate(params[:email], params[:password])
+
+    if @user.present?
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: 'Вы успешно залогинились'
+    else
+      flash.now.alert = 'Неправильная почта или пароль'
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: 'Вы разлогинились! Приходите еще!'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   root to: 'users#index'
 
   resources :users, except: [:destroy]
+  resources :sessions, only: %i[new create destroy]
   resources :questions
+
+  get 'sign_up' => 'users#new'
+  get 'log_out' => 'sessions#destroy'
+  get 'log_in' => 'sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
