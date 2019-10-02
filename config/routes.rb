fc2aca83c962db5286c90ad8ac8e5749cfea8fd3
Dmitrii Iurin <dimon07710@yandex.ru>
2019-10-02 21:31:41 +0300

    исправлена ошибка безопасности

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,11 @@
 Rails.application.routes.draw do
   root to: 'users#index'
 
+  resource :session, only: %i[new create destroy]
+
   resources :users
-  resources :sessions, only: %i[new create destroy]
   resources :hashtags, only: %i[show]
   resources :questions, except: %i[show new index]
 
-  get 'sign_up' => 'users#new'
-  get 'log_out' => 'sessions#destroy'
-  get 'log_in' => 'sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
